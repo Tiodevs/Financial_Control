f90fa8bd16a2a17dc58eb6ffa0be26f3c2134952
Felipe Pereira dos Santos <87885399+Tiodevs@users.noreply.github.com>
2021-11-28 19:52:44 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,24 +16,7 @@ const Modal = {
 
 const Transaction = {
     all: [
-        {
-            id: 1,
-            description: 'Luz',
-            amount: - 50000,
-            date: '23/01/2021',
-        },
-        {
-            id:2,
-            description: 'Website',
-            amount: 500000,
-            date: '23/01/2021',
-        },
-        {
-            id:3,
-            description: 'Internet',
-            amount: - 20000,
-            date: '23/01/2021',
-        },
+       
     ],
     add(transaction){
         //Vai adicionara a trancição na tabela
